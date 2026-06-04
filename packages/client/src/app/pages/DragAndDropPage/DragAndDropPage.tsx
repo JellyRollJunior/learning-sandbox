34cd30b2bf1d8e49890cc4557dbe2d9279dc0f4c
Brandon Lin <brandonlin9716@gmail.com>
2026-06-04 15:43:16 -0700

    Add link from homepage to drag and drop page

--- packages/client/src/app/pages/DragAndDropPage/DragAndDropPage.tsx
+++ packages/client/src/app/pages/DragAndDropPage/DragAndDropPage.tsx
@@ -1,9 +1,11 @@
 const DragAndDropPage = () => {
   return (
     <>
-      <h1 className="mt-5 text-center text-xl font-bold">
-        Welcome to the Drag and Drop page!
-      </h1>
+      <main className="min-h-screen min-w-screen">
+        <h1 className="mt-5 text-center text-xl font-bold">
+          Welcome to the Drag and Drop page!
+        </h1>
+      </main>
     </>
   );
 };
--- packages/client/src/app/pages/HomePage/HomePage.tsx
+++ packages/client/src/app/pages/HomePage/HomePage.tsx
@@ -1,9 +1,19 @@
 const HomePage = () => {
   return (
     <>
-      <h1 className="mt-5 text-center text-xl font-bold">
-        Welcome to the Learning Sandbox!
-      </h1>
+      <div className="min-h-screen min-w-screen">
+        <h1 className="mt-5 text-center text-xl font-bold">
+          Welcome to the Learning Sandbox!
+        </h1>
+        <main className="mx-5 mt-5 grid min-h-25 grid-cols-3 rounded-lg border-2 border-purple-500 px-2 py-3">
+          <a
+            className="h-fit rounded-sm border-2 border-purple-800 text-center"
+            href="/drag-and-drop"
+          >
+            Drag and Drop
+          </a>
+        </main>
+      </div>
     </>
   );
 };
