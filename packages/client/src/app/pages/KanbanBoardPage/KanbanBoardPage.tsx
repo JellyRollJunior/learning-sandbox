c2631d371ceb7a63bc86a00ff9a96fdccaa9df81
Brandon Lin <brandonlin9716@gmail.com>
2026-06-06 16:01:35 -0700

    Render board data

--- packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
+++ packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
@@ -11,8 +11,25 @@ const KanbanBoardPage = () => {
         <main className='flex-1'>
           <div className='mx-5 mt-5 h-full rounded-lg border-2 border-black p-2'>
             <h2 className='text-center text-md'>This is the board</h2>
+
+            {/* Board */}
             <ul className='w-full flex border border-black p-2 items-center justify-center gap-6'>
-              
+
+              {/* Sections */}
+              {data.map((section: Section) => (
+                <li className='min-w-50 min-h-50 border border-blue'>
+                  <h3 className='text-center'>{section.id} : {section.name}</h3>
+
+                  {/* items */}
+                  <ol className='flex flex-col p-2 gap-2'>
+                    {section.items.map((item: Item) => (
+                      <li className='pl-2 border border-black'>
+                        {item.id} : {item.name}
+                      </li>
+                    ))}
+                  </ol>
+                </li>
+              ))}
             </ul>
           </div>
         </main>
