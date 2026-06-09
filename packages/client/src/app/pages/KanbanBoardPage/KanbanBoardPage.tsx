b74f5c8c801af2e9961c1c6c2a004026f5f1aa17
Brandon Lin <brandonlin9716@gmail.com>
2026-06-09 15:49:39 -0700

    Extract SectionItem. Set isDragging on SectionItem

--- packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
+++ packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
@@ -1,31 +1,70 @@
 import type { Item, Section } from './data.ts';
+import { useCallback, useRef, type PointerEvent } from 'react';
 import { data } from './data.ts';
+
+type SectionItemProps = { item: Item };
+const SectionItem = ({ item }: SectionItemProps) => {
+  const itemRef = useRef<HTMLLiElement | null>(null);
+  const isDragging = useRef(false);
+
+  const onPointerDown = useCallback((event: PointerEvent) => {
+    isDragging.current = true;
+    event.currentTarget.setPointerCapture(event.pointerId);
+    
+    console.log(`${item.id}: ${isDragging.current}`);
+  }, [item]);
+
+  const onPointerMove = useCallback((event: PointerEvent) => {
+    if (!isDragging.current) return;
+    
+  }, []);
+
+  const onPointerUp = useCallback((event: PointerEvent) => {
+    isDragging.current = false;
+    console.log(`${item.id}: ${isDragging.current}`);
+  }, [item]);
+
+  return (
+    <li
+      className="border border-black pl-2"
+      ref={itemRef}
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
+      onPointerUp={onPointerUp}
+    >
+      {item.id} : {item.name}
+    </li>
+  );
+};
 
 const KanbanBoardPage = () => {
   return (
     <>
-      <div className='min-h-screen flex flex-col'>
+      <div className="flex min-h-screen flex-col">
         <h1 className="mt-5 text-center text-xl font-bold">
           Welcome to the Kanban Board Page!
         </h1>
-        <main className='flex-1'>
-          <div className='mx-5 mt-5 h-full rounded-lg border-2 border-black p-2'>
-            <h2 className='text-center text-md'>This is the board</h2>
+        <main className="flex-1">
+          <div className="mx-5 mt-5 h-full rounded-lg border-2 border-black p-2">
+            <h2 className="text-md text-center">This is the board</h2>
 
             {/* Board */}
-            <ul className='w-full flex border border-black p-2 items-center justify-center gap-6'>
-
+            <ul className="flex w-full items-center justify-center gap-6 border border-black p-2">
+              
               {/* Sections */}
               {data.map((section: Section) => (
-                <li className='min-w-50 min-h-50 border border-blue'>
-                  <h3 className='text-center'>{section.id} : {section.name}</h3>
+                <li
+                  key={section.id}
+                  className="border-blue min-h-50 min-w-50 border"
+                >
+                  <h3 className="text-center">
+                    {section.id} : {section.name}
+                  </h3>
 
                   {/* items */}
-                  <ol className='flex flex-col p-2 gap-2'>
+                  <ol className="flex flex-col gap-2 p-2">
                     {section.items.map((item: Item) => (
-                      <li className='pl-2 border border-black'>
-                        {item.id} : {item.name}
-                      </li>
+                      <SectionItem key={item.id} item={item} />
                     ))}
                   </ol>
                 </li>
