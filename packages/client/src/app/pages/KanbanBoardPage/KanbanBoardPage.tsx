de1c904c8eec425efcb9fdd3044ed7a783d39dc3
Brandon Lin <brandonlin9716@gmail.com>
2026-06-13 16:23:46 -0700

    Implement ghost that follows cursor on dragging section item

--- packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
+++ packages/client/src/app/pages/KanbanBoardPage/KanbanBoardPage.tsx
@@ -1,79 +1,88 @@
+import { useCallback, useState, type PointerEvent } from 'react';
+import { createPortal } from 'react-dom';
 import type { Item, Section } from './data.ts';
-import { useCallback, useRef, type PointerEvent } from 'react';
 import { data } from './data.ts';
 
-type SectionItemProps = { item: Item };
-const SectionItem = ({ item }: SectionItemProps) => {
-  const itemRef = useRef<HTMLLiElement | null>(null);
-  const isDragging = useRef(false);
+interface Position {
+  x: number;
+  y: number;
+}
+
+type boardProps = { data: Section[] };
+const Board = ({ data }: boardProps) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [pos, setPos] = useState<Position>({ x: 0, y: 0 });
 
   const onPointerDown = useCallback((event: PointerEvent) => {
-    isDragging.current = true;
+    setIsDragging(true);
     event.currentTarget.setPointerCapture(event.pointerId);
-    
-    console.log(`${item.id}: ${isDragging.current}`);
-  }, [item]);
-
-  const onPointerMove = useCallback((event: PointerEvent) => {
-    if (!isDragging.current) return;
-    
+    setPos({ x: event.clientX, y: event.clientY });
   }, []);
 
-  const onPointerUp = useCallback((event: PointerEvent) => {
-    isDragging.current = false;
-    console.log(`${item.id}: ${isDragging.current}`);
-  }, [item]);
+  const onPointerMove = useCallback((event: PointerEvent) => {
+    if (isDragging == false) return;
+
+    setPos({ x: event.clientX, y: event.clientY });
+  }, [isDragging]);
+
+  const onPointerUp = useCallback(() => {
+    setIsDragging(false);
+  }, []);
 
   return (
-    <li
-      className="border border-black pl-2"
-      ref={itemRef}
-      onPointerDown={onPointerDown}
-      onPointerMove={onPointerMove}
-      onPointerUp={onPointerUp}
-    >
-      {item.id} : {item.name}
-    </li>
+    <>
+      {/* Board */}
+      <ul className="flex w-full items-center justify-center gap-6 border border-black p-2">
+        {/* Sections */}
+        {data.map((section) => (
+          <li key={section.id} className="border-blue min-h-50 min-w-50 border">
+            <h3 className="text-center">
+              {section.id} : {section.name}
+            </h3>
+
+            {/* items */}
+            <ol className="flex flex-col gap-2 p-2">
+              {section.items.map((item: Item) => (
+                <li
+                  className="border border-black px-2 py-1 select-none"
+                  onPointerDown={onPointerDown}
+                  onPointerMove={onPointerMove}
+                  onPointerUp={onPointerUp}
+                >
+                  {item.id} : {item.name}
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ul>
+
+      {/* Dragging Ghost */}
+      {isDragging &&
+        createPortal(
+          <div
+            className="pointer-events-none absolute h-5 w-10 bg-blue-200"
+            style={{ left: pos.x, top: pos.y }}
+          ></div>,
+          document.body
+        )}
+    </>
   );
 };
 
 const KanbanBoardPage = () => {
   return (
-    <>
-      <div className="flex min-h-screen flex-col">
-        <h1 className="mt-5 text-center text-xl font-bold">
-          Welcome to the Kanban Board Page!
-        </h1>
-        <main className="flex-1">
-          <div className="mx-5 mt-5 h-full rounded-lg border-2 border-black p-2">
-            <h2 className="text-md text-center">This is the board</h2>
-
-            {/* Board */}
-            <ul className="flex w-full items-center justify-center gap-6 border border-black p-2">
-              
-              {/* Sections */}
-              {data.map((section: Section) => (
-                <li
-                  key={section.id}
-                  className="border-blue min-h-50 min-w-50 border"
-                >
-                  <h3 className="text-center">
-                    {section.id} : {section.name}
-                  </h3>
-
-                  {/* items */}
-                  <ol className="flex flex-col gap-2 p-2">
-                    {section.items.map((item: Item) => (
-                      <SectionItem key={item.id} item={item} />
-                    ))}
-                  </ol>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </main>
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col">
+      <h1 className="mt-5 text-center text-xl font-bold">
+        Welcome to the Kanban Board Page!
+      </h1>
+      <main className="flex-1">
+        <div className="mx-5 mt-5 h-full rounded-lg border-2 border-black p-2">
+          <h2 className="text-md text-center">This is the board</h2>
+          <Board data={data} />
+        </div>
+      </main>
+    </div>
   );
 };
 
