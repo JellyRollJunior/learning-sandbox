e869c9eafe59366e618b92b2a23f205f5df15630
Brandon Lin <brandonlin9716@gmail.com>
2026-07-10 20:52:48 -0700

    Create drop area for dragged items

--- packages/client/src/app/pages/KanbanBoardPage2/KanbanBoardPage2.tsx
+++ packages/client/src/app/pages/KanbanBoardPage2/KanbanBoardPage2.tsx
@@ -1,13 +1,24 @@
-import { useState, type DragEventHandler } from 'react';
+import { Fragment, useState, type DragEventHandler } from 'react';
 import type { Item, Section } from './data.ts';
 import { data } from './data.ts';
+
+type DropAreaProps = { onDropHandler: DragEventHandler };
+const DropArea = ({ onDropHandler }: DropAreaProps) => {
+  return (
+    <li
+      className="flex h-10 w-full items-center justify-center rounded-sm border border-black"
+      onDrop={onDropHandler}
+    >
+      Drop Area
+    </li>
+  );
+};
 
 type ItemProps = { item: Item; onDragStartHandler: DragEventHandler };
 const Item = ({ item, onDragStartHandler }: ItemProps) => {
   return (
     <li
       className="border border-black px-2 py-1 select-none"
-      key={item.id}
       onDragStart={onDragStartHandler}
       draggable
     >
@@ -36,10 +47,13 @@ const Board = ({ data }: BoardProps) => {
               <ol className="flex flex-col gap-2 p-2">
                 {/* items */}
                 {section.items.map((item: Item) => (
-                  <Item
-                    item={item}
-                    onDragStartHandler={() => setActiveItem(item.id)}
-                  />
+                  <Fragment key={item.id}>
+                    <Item
+                      item={item}
+                      onDragStartHandler={() => setActiveItem(item.id)}
+                    />
+                    <DropArea onDropHandler={() => console.log('hello')} />
+                  </Fragment>
                 ))}
               </ol>
             </li>
--- packages/client/src/app/pages/KanbanBoardPage2/data.ts
+++ packages/client/src/app/pages/KanbanBoardPage2/data.ts
@@ -1,27 +1,30 @@
-type Item = { id: number; name: string };
-type Section = { id: number; name: string; items: Item[] };
+type Item = { id: number; order: string; name: string };
+type Section = { id: number; order: string; name: string; items: Item[] };
 
 const data: Section[] = [
     {
         id: 100,
+        order: '0', 
         name: 'section 1',
         items: [
-            { id: 1, name: 'hello' },
-            { id: 2, name: 'bye' },
-            { id: 3, name: 'swag' },
+            { id: 1, order: '0', name: 'hello' },
+            { id: 2, order: '1', name: 'bye' },
+            { id: 3, order: '2', name: 'swag' },
         ],
     },
     {
         id: 101,
+        order: '1', 
         name: 'section 2',
         items: [
-            { id: 4, name: 'zoopy' },
-            { id: 5, name: 'zippy' },
-            { id: 6, name: 'zawpy' },
+            { id: 4, order: '0', name: 'zoopy' },
+            { id: 5, order: '1', name: 'zippy' },
+            { id: 6, order: '2', name: 'zawpy' },
         ],
     },
     {
         id: 102,
+        order: '2', 
         name: 'section 3',
         items: [],
     },
